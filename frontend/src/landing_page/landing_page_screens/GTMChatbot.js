4516202a69191f88954a7dd04aa308f0e95d4ba4
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2025-06-20 13:34:06 -0400

    trying to implement chatbot component

--- frontend/src/landing_page/landing_page_screens/GTMChatbot.js
+++ frontend/src/landing_page/landing_page_screens/GTMChatbot.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Chatbot from "./../landing_page_components/Chatbot/Chatbot.js"
 
-function GTMChatbot() {
+const GTMChatbot = ()  => {
   return (
     <div className="text-white text-center mt-10">
       <h1 className="text-4xl font-bold mb-4">GTM Chatbot</h1>
       <p>This is your go-to-market chatbot builder page!</p>
+      <Chatbot />
     </div>
   );
 }
